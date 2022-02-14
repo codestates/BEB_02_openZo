c262217e9c38e267be977d26a88b50bb00604a46
2now <kj109888@gmail.com>
2022-02-14 20:51:14 +0900

    🔨 Add Search page
    Add Search page and banner, form

---
+++ front/src/components/SearchForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Button, Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+
+const SearchWrapper = styled.div`
+  display: flex;
+  width: 25rem;
+  margin-bottom: 0.5rem;
+  margin-left: 3.5rem;
+`;
+
+const StyledSearchInput = styled.div`
+  width: 100%;
+  border-bottom: 1.5px solid;
+  border-color: rgba(57, 102, 249, 0.5);
+  margin-right: 0.3rem;
+`;
+
+export default function SearchForm({ setSearchWord }) {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleClick = () => {
+    setSearchWord(searchInput);
+    setSearchInput('');
+  };
+
+  return (
+    <SearchWrapper>
+      <StyledSearchInput>
+        <Input
+          placeholder="Search NFTs"
+          bordered={false}
+          value={searchInput}
+          onChange={handleInputChange}
+        />
+      </StyledSearchInput>
+      <Link to="/search">
+        <Button
+          shape="circle"
+          onClick={handleClick}
+          icon={<SearchOutlined />}
+        />
+      </Link>
+    </SearchWrapper>
+  );
+}
