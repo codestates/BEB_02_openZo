5991469168c41f21d97f1808994703cd304ca319
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2022-02-15 21:54:01 +0900

    :memo: update contract && redeploy

--- contract/contracts/OpzNFTs.sol
+++ contract/contracts/OpzNFTs.sol
@@ -1,4 +1,3 @@
-//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
@@ -6,21 +5,50 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract OpzNFTs is ERC721URIStorage, Ownable {
+contract OpzNFTs is ERC721URIStorage, Ownable, ERC721Enumerable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() public ERC721("OpzNFTs", "OpzNFT") {}
 
-    function mintNFT(string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal
+      override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(
+        uint256 tokenId
+    ) internal
+      override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+  function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
     {
+        return super.supportsInterface(interfaceId);
+    }
+    function tokenURI(
+        uint256 tokenId
+    ) public view
+      override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+    
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
-        _mint(msg.sender, newItemId);
+        _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
