af6982cd591ad81efe1b604774c3bbb6330a9b33
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2022-02-15 18:56:57 +0900

    :twisted_rightwards_arrows: merge05

--- front/src/pages/Explore.js
+++ front/src/pages/Explore.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import NftList from '../components/NftList';
-import PageBanner from '../components/PageBanner';
+import NftList from '../components/nft/NftList';
+import PageBanner from '../components/banner/PageBanner';
 
 export default function Explore() {
   // TODO: search 하면 받은 list로 NFT List 넘겨줘서 view
